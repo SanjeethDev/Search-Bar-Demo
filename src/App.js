cd4a0ee56ecdabef6aacd7488f1ab58f77b259b4
SanjeethDev <91370632+SanjeethDev@users.noreply.github.com>
2023-06-25 02:10:35 +0530

    Changes to designs Implemented search filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(null);
+  const [search, setSearch] = useState('');
+  console.log(search)
+  console.log(data);
+
+  function ListItems(props) {
+    return (
+      <>
+        <img src={props.avatar} alt={props.first_name}></img>
+        <p>{props.first_name}</p>
+        <span>{props.id}</span>
+      </>
+    )
+  }
+
+  async function pullJson() {
+    const reponse = await fetch('https://reqres.in/api/users?page=1')
+    const responseData = await reponse.json()
+    setData(responseData.data)   
+  }
+
+  useEffect(() => {
+    pullJson()
+  }, [])
+  
+  if (data == null) {
+    return (
+      <div className="App">
+        <div className="search_container">
+          <input type='text' placeholder='Search' onChange={(e) => setSearch(e.target.value)}/>
+        </div>
+        <div className='list_container'>
+            
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <div className="search_container">
+        <input type='text' placeholder='Search' onChange={(e) => setSearch(e.target.value)}/>
+        </div>
+        <div className='list_container'>
+            {data.filter((mappedObject, index) => {return (
+              search.toLowerCase() === '' ? mappedObject : mappedObject.first_name.toLowerCase().includes(search.toLowerCase())
+            )}).map((mappedObject, index) => {
+                return (
+                  <div className='items_container' key={index}>
+                    <ListItems avatar = {mappedObject.avatar} first_name = {mappedObject.first_name} id = {mappedObject.id}></ListItems>
+                  </div>
+                )
+              })
+            }
+        </div>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
